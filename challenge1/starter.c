1ee5596df8ca4a2f8e576c2b957dadeb2b626eb7
alguna.jviladoms <alguna@jviladoms.cat>
2025-12-04 09:44:52 +0100

    añado strater.c de challenge 1

---
+++ challenge1/starter.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int suma (int a, int b) {
+
+    int suma = a + b;
+    return suma;
+}
+
+
+void main () {
+
+    int a, b;
+
+    printf("Dime un numero a\n");
+    scanf(" %d", &a);
+    printf("Dime un numero b");
+    scanf(" %d", &b);
+
+    printf("El resultado es %d\n", suma (a, b));
+
+}
